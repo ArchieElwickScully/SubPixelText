f4b8a06e618b9625792ce66299a3f6cf3b0a1a23
Archie <archiees@icloud.com>
2024-11-22 21:28:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from PIL import Image
+import os
 
 
 def main():
-    print('hi')
+    text = input("Enter text to convert to subpixel image: ")
+    imgname = text
+    i = 1
+    
+    while os.path.isfile(imgname + ".png"):
+        imgname = imgname + str(i)
+        i += i
+
+    with open(imgname + ".png", "w") as f:
+        print("file created, writing to file")
+
+    
+    
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     main()
