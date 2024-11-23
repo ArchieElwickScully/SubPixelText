1f4d587379ffce0bafc2cc5745981019fcbf3cf8
Archie <archiees@icloud.com>
2024-11-23 13:51:18 +0000

    working, just need to finish list of letters

--- main.py
+++ main.py
@@ -2,30 +2,63 @@ from PIL import Image
 import numpy as np
 import os
 
-def createImage(text):
-    pixels = [[(255, 0, 255), (0, 255, 0)],
-              [(255, 255, 255), (0, 255, 0)],
-              [(255, 0, 255), (0, 255, 0)]]
+class Letters:
+    def __init__(self):
+        self.letters = {"A" :
+                 {"top" : [0, 1, 0],
+                  "mto" : [1, 0, 1],
+                  "mbo" : [1, 1, 1],
+                  "bot" : [1, 0, 1]
+                  }
+        }
+
+    def get(self, letter, pos):
+        return self.letters[letter.upper()][pos]
+
+def getLetters(text, brightness):
+    letters = Letters()
+    
+    positions = ["top", "mto", "mbo", "bot"]
+    
+    pixels = []
+    currentRow = []
+    currentPixel = ()
+
+    for pos in positions:
+        for l in text:
+            for subpixel in letters.get(l, pos):
+                if subpixel:
+                    currentPixel += (brightness,)
+                else:
+                    currentPixel += (0,)
+
+            currentRow.append(currentPixel)
+            currentPixel = ()
+                            
+        pixels.append(currentRow)
+        currentRow = []
+
+    return pixels        
+        
+def createImage(text, name):
+    pixels = getLetters(text, 255)
 
     array = np.array(pixels, dtype=np.uint8)
 
     image = Image.fromarray(array)
-    image.save(text + ".png")
+    image.save(name + ".png")
 
 
 def main():
     text = input("Enter text to convert to subpixel image: ")
-    imgname = text
+    imgname = "img/" + text
     i = 1
     
     while os.path.isfile(imgname + ".png"):
         imgname = imgname + str(i)
         i += i
 
-    
-    createImage(imgname)
-
-    
+    createImage(text, imgname)
         
 
 
